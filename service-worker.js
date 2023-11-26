836161fa2b14a44b74b6d2724fa9769663a9cda7
Lackmake <kevinlackmann.kl@gmail.com>
2023-11-26 17:32:40 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('work-time-calculator-v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+        //   '/icon.png', // Include the path to your icon file
+          // Add other static assets as needed
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
